21cd25e8a5348ba772b1ea431fc8d83736f9157c
Igiraneza1 <igiranezaadeline@gmail.com>
2025-07-26 11:44:49 +0300

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Login(){
+
+    return(
+        <div className="grid grid-cols-2  bg-gray-100">
+            <div className="bg-blue-500  flex justify-center items-center">
+                <h1>Welcome User!</h1>
+
+            </div>
+
+        </div>
+    )
+}
